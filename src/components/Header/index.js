1501073d3aeab2f61030b8b7ab1d111827549db4
Ramalho <jonathan.oliveira.ramalho@gmail.com>
2020-06-09 23:46:20 -0300

    Dumb commits

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import useStyles from './styles';
+
+const Header = ({}) => {
+    const classes = useStyles();
+    return(
+        <div className={classes.root}>
+            <Avatar alt="Remy Sharp" src="https://avatars2.githubusercontent.com/u/7648741?s=460&u=f019ed9e57792d11b69b7dc689f9fdda1998ad2a&v=4" />
+            <a>Pwa do Ramalho</a>
+        </div>
+    )
+}
+
+export default Header;
