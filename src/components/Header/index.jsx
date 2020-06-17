c4e7d74818e0a212ec59c85e3f37470df8df665c
Ramalho <jonathan.oliveira.ramalho@gmail.com>
2020-06-17 00:22:11 -0300

    warning fix

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -7,7 +7,6 @@ const Header = () => {
     return(
         <header className={classes.root}>
             <Avatar alt="Remy Sharp" src="https://avatars2.githubusercontent.com/u/7648741?s=460&u=f019ed9e57792d11b69b7dc689f9fdda1998ad2a&v=4" />
-            <a>Pwa do Ramalho</a>
         </header>
     )
 }
